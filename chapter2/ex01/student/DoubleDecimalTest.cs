e5242b3bd46a24932b019279a111b871a888c08a
Mypham014 <hmyp014@gmail.com>
2025-02-10 05:46:46 +0000

    Testing Mon Feb 10 2025 05:46:46 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/DoubleDecimalTest.cs
+++ chapter2/ex01/student/DoubleDecimalTest.cs
@@ -5,7 +5,12 @@ class InchesToCentimeters
 {
 	static void Main()
 	{
+		const float inchtocm = 2.54;
 		float inches = 3;
-		float cm = inches * 2.54;
+		float cm = inches * inchtocm;
+		WriteLine(inches);
+		Write(" inches is ")
+		WriteLine(cm);
+		Write(" centimeters");
 	}
 }
