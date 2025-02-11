f65a1290a5f2641d1dc4c453d692fdbb726e9864
Mypham014 <hmyp014@gmail.com>
2025-02-11 07:56:01 +0000

    Testing Tue Feb 11 2025 07:56:01 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/CheckCredit.cs
+++ chapter4/ex01/student/CheckCredit.cs
@@ -15,12 +15,12 @@ class PersonalInfo
 {
 	static void Main()
 	{
-		Console.WriteLine("```");
-		Console.WriteLine("");
-		Console.WriteLine("Lucinda Potter");
-		Console.WriteLine("6/24/1992");
-		Console.WriteLine("work 000-555-0101");
-		Console.WriteLine("cell 000-555-0189");
-		Console.WriteLine("```");
+		WriteLine("```");
+		Write("");
+		WriteLine("Lucinda Potter");
+		WriteLine("6/24/1992");
+		WriteLine("work 000-555-0101");
+		WriteLine("cell 000-555-0189");
+		WriteLine("```");
 	}
 }
