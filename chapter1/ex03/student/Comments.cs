ee2a5b6fca9d030ccb79b46223fc9003fb3ac5b8
Mypham014 <hmyp014@gmail.com>
2025-02-11 01:44:09 -0600

    Update Comments.cs

--- chapter1/ex03/student/Comments.cs
+++ chapter1/ex03/student/Comments.cs
@@ -6,8 +6,8 @@ class Comments
 	static void Main()
 	{
 		WriteLine(" Two way to write out comments");
-		// Let a line become comment by adding two slashes
-		/* Let a block become comment by adding a slash and 
+		// Let a line becomes comment by adding two slashes
+		/* Let a block becomes comment by adding a slash and 
 		an asterisk at the beginning and an asterisk and a 
 		slash at the end */ 
 	}
