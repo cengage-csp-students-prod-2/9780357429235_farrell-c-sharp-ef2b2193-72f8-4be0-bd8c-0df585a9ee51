a3b3ce1c7d02a4d0d3f2b0e26ea0d0151a008d55
Mypham014 <hmyp014@gmail.com>
2025-02-11 01:45:20 -0600

    Update BurmaShave.cs

--- chapter1/ex06/student/BurmaShave.cs
+++ chapter1/ex06/student/BurmaShave.cs
@@ -5,6 +5,31 @@ class BurmaShave
 {
 	static void Main()
 	{
-		// Write your code here
+		WriteLine("	  1929");
+		WriteLine("	Every shaver");
+		WriteLine("	Now can snore");
+		WriteLine("	Six more minutes");
+		WriteLine("	Than before");
+		WriteLine("	By using");
+		WriteLine("	Burma-Shave");
+		WriteLine("	Your shaving brush");
+		WriteLine("	Has had its day");
+		WriteLine("	So why not");
+		WriteLine("	Shave the modern way");
+		WriteLine("	With");
+		WriteLine("	Burma-Shave");
+		WriteLine("");
+		WriteLine("	Two");
+		WriteLine("	Hundred");
+		WriteLine("	Thousand");
+		WriteLine("	Men");
+		WriteLine("	Use");
+		WriteLine("	Burma-Shave");
+		WriteLine("");
+		WriteLine("	Half a poundFor");
+		WriteLine("	Half a dollar");
+		WriteLine("	Spread on thin");
+		WriteLine("	Above the collar");
+		WriteLine("	Burma-Shave");
 	}
 }
