ecd7cb0b4ec37d23b9ab83b75642de8102fa2f33
Mypham014 <hmyp014@gmail.com>
2025-02-11 07:55:22 +0000

    Testing Tue Feb 11 2025 07:55:22 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/CheckCredit.cs
+++ chapter4/ex01/student/CheckCredit.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using static System.Console;
 using System.Globalization;
 class CheckCredit
@@ -6,5 +6,21 @@ class CheckCredit
 	static void Main()
 	{
 		// Write your code here
+	}
+}*/
+using System;
+using static System.Console;
+using System.Globalization;
+class PersonalInfo
+{
+	static void Main()
+	{
+		Console.WriteLine("```");
+		Console.WriteLine("");
+		Console.WriteLine("Lucinda Potter");
+		Console.WriteLine("6/24/1992");
+		Console.WriteLine("work 000-555-0101");
+		Console.WriteLine("cell 000-555-0189");
+		Console.WriteLine("```");
 	}
 }
