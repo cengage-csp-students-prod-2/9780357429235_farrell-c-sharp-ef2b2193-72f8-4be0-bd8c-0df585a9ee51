8bac324facc537bcc2c58952f403812e0d0b8c64
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:18:03 -0600

    Update GreenvilleRevenue.cs

--- chapter2/cs01/student/GreenvilleRevenue.cs
+++ chapter2/cs01/student/GreenvilleRevenue.cs
@@ -5,6 +5,20 @@ class GreenvilleRevenue
 {
    static void Main()
    {
-      // Write your code here
+    // collect data
+     Write("Enter number of contestants last year >> ");
+     int contestantsLastYear = Convert.ToInt32(ReadLine());
+     Write("Enter number of contestants this year >> ");
+     int contestantsThisYear = Convert.ToInt32(ReadLine());
+    
+    // calculate revenue
+    double revenueThisYear = contestantsThisYear * 25;
+    double revenueLastYear = contestantsLastYear * 25;
+    bool revenueIncreased = revenueThisYear > revenueLastYear;
+    
+   // output
+    WriteLine("Last year's competition had {0} contestants, and this year's has {1} contestants", contestantsLastYear, contestantsThisYear);
+    WriteLine("Revenue expected this year is {0}", revenueThisYear.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+    WriteLine("It is {0} that this year's competition is bigger than last year's", revenueIncreased);
    }
 }
