3e91d6709f952e5e7059e01fbda7e473dc6f1317
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:24:37 -0600

    Update Planets.cs

--- chapter2/ex14/student/Planets.cs
+++ chapter2/ex14/student/Planets.cs
@@ -3,8 +3,22 @@ using static System.Console;
 using System.Globalization;
 class Planets
 {
+    enum SolarPlanet
+        {
+            MERCURY = 1,
+            VENUS = 2,
+            EARTH = 3,
+            MARS = 4,
+            JUPITER = 5,
+            SATURN = 6,
+            URANUS = 7,
+            NEPTUNE = 8
+        }
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter a planet number >> ");
+        int planetNumber = int.Parse(ReadLine());
+        
+        WriteLine("{0} is {1} planet(s) from the sun ", (SolarPlanet)planetNumber, planetNumber);
 	}
 }
