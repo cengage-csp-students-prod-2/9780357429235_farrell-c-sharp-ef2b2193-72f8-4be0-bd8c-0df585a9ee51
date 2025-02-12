4d6a66f0d6570a35cc3a803400172590b8f7ec0c
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:24:51 -0600

    Update PigLatin.cs

--- chapter2/ex15/student/PigLatin.cs
+++ chapter2/ex15/student/PigLatin.cs
@@ -5,6 +5,12 @@ class PigLatin
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter a word >> ");
+        string word = ReadLine();
+
+        string firstLetter = word.Substring(0, 1);
+        string WordLeft = word.Substring(1, word.Length - 1);
+        
+        WriteLine("{0} in Pig Latin is {1}{2}ay", word, WordLeft, firstLetter);
 	}
 }
