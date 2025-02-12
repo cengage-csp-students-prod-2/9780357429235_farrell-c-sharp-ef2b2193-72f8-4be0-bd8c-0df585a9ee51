186d5d2d02ab1a26d3b755c7e192ffcd88a1f08e
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:24:16 -0600

    Update MonthNames.cs

--- chapter2/ex13/student/MonthNames.cs
+++ chapter2/ex13/student/MonthNames.cs
@@ -3,8 +3,28 @@ using static System.Console;
 using System.Globalization;
 class MonthNames
 {
+    enum Months
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12
+        }
 	static void Main()
 	{
-		// Write your code here
+
+        Write("Enter a month number >> ");
+        int monthNumber = int.Parse(ReadLine());
+
+        WriteLine("The month is {0}", (Months)monthNumber);
+
 	}
 }
