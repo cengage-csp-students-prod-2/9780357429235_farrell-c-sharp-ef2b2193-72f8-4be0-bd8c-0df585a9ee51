44fe60fdadfcfc2e5088ab5a673a0c0279491a08
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:22:31 -0600

    Update Eggs.cs

--- chapter2/ex08/student/Eggs.cs
+++ chapter2/ex08/student/Eggs.cs
@@ -5,6 +5,15 @@ class Eggs
 {
 	static void Main()
 	{
-		// Write your code here
+		int chicken1 = 9;
+        int chicken2 = 7;
+        int chicken3 = 6;
+        int chicken4 = 105;
+
+        int totalEggs = chicken1 + chicken2 + chicken3 + chicken4;
+        int dozen = totalEggs / 12;
+        int eggLeft = totalEggs % 12;
+
+        WriteLine("{0} eggs is {1} dozen and {2} eggs", totalEggs, dozen, eggLeft);
 	}
 }
