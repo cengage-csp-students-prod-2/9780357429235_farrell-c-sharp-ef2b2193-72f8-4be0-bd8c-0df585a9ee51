3cf515f21afe8b7148001e996381c4a1fa89bdf9
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:22:45 -0600

    Update EggsInteractive.cs

--- chapter2/ex09/student/EggsInteractive.cs
+++ chapter2/ex09/student/EggsInteractive.cs
@@ -5,6 +5,19 @@ class EggsInteractive
 {
 	static void Main()
 	{
-		// Write your code here
+        WriteLine("Enter the number of eggs for chicken 1 >> ");
+		int chicken1 = int.Parse(ReadLine());
+        WriteLine("Enter the number of eggs for chicken 2 >> ");
+        int chicken2 = int.Parse(ReadLine());
+        WriteLine("Enter the number of eggs for chicken 3 >> ");
+        int chicken3 = int.Parse(ReadLine());
+        WriteLine("Enter the number of eggs for chicken 4 >> ");
+        int chicken4 = int.Parse(ReadLine());
+
+        int totalEggs = chicken1 + chicken2 + chicken3 + chicken4;
+        int dozen = totalEggs / 12;
+        int eggLeft = totalEggs % 12;
+
+        WriteLine("{0} eggs is {1} dozen and {2} eggs", totalEggs, dozen, eggLeft);
 	}
 }
