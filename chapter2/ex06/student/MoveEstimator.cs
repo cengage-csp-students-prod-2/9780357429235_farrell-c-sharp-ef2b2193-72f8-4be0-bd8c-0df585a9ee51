81bc14131ee9d8daea88b7be5981a2916d1974fa
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:21:56 -0600

    Update MoveEstimator.cs

--- chapter2/ex06/student/MoveEstimator.cs
+++ chapter2/ex06/student/MoveEstimator.cs
@@ -5,6 +5,18 @@ class MoveEstimator
 {
 	static void Main()
 	{
-		// Write your code here
-	}
+		const double baseRate = 200.0;
+        double Rate_Per_hour = 150.0;
+        double Rate_Per_Mile = 2.0;
+
+        Write("Enter the number of hours >> ");
+        double hour = double.Parse(ReadLine());
+        Write("Enter the number of miles >> ");
+        double mile = double.Parse(ReadLine());
+
+        double estimate = baseRate + (Rate_Per_hour * hour) + (Rate_Per_Mile * mile);
+
+        WriteLine("*For a move taking {0} hours and going {1} miles the estimate is {2}", hour, mile, estimate.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
+    }
 }
