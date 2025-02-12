501a5a24e5c52d713cf30755f578a1776068c5a6
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:21:34 -0600

    Update ProjectedRaises.cs

--- chapter2/ex05/student/ProjectedRaises.cs
+++ chapter2/ex05/student/ProjectedRaises.cs
@@ -5,6 +5,18 @@ class ProjectedRaises
 {
 	static void Main()
 	{
-		// Write your code here
+		const double RAISE = 0.04;
+        double employee1 = 25000, employee2 = 38000, employee3 = 51000;
+        WriteLine("The salary of employee 1 is {0}", employee1.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("The salary of employee 2 is {0}", employee2.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("The salary of employee 3 is {0}", employee3.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
+        employee1 += employee1 * RAISE;
+        employee2 += employee2 * RAISE;
+        employee3 += employee3 * RAISE;
+
+        WriteLine("Next year's salary for the first employee will be {0}",  employee1.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("Next year's salary for the second employee will be {0}", employee2.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("Next year's salary for the third employee will be {0}", employee3.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 	}
 }
