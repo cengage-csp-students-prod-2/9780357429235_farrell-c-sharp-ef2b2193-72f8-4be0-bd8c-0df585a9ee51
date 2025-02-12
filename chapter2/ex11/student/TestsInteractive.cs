ff3771a27c8d88408688c1ee7c49c45d39b2387e
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:23:33 -0600

    Update TestsInteractive.cs

--- chapter2/ex11/student/TestsInteractive.cs
+++ chapter2/ex11/student/TestsInteractive.cs
@@ -5,6 +5,24 @@ class TestsInteractive
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter test score 1 >> ");
+        int score1 = int.Parse(ReadLine());
+        Write("Enter test score 2 >> ");
+        int score2 = int.Parse(ReadLine());
+        Write("Enter test score 3 >> ");
+        int score3 = int.Parse(ReadLine());
+        Write("Enter test score 4 >> ");
+        int score4 = int.Parse(ReadLine());
+        Write("Enter test score 5 >> ");
+        int score5 = int.Parse(ReadLine());
+        Write("Enter test score 6 >> ");
+        int score6 = int.Parse(ReadLine());
+        Write("Enter test score 7 >> ");
+        int score7 = int.Parse(ReadLine());
+        Write("Enter test score 8 >> ");
+        int score8 = int.Parse(ReadLine());
+
+        double average = (score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8) / 8.0;
+        WriteLine("The average test scores is {0}", average.ToString("0.00"));
 	}
 }
