9477699627a492c3c66f925f6282dc30a09605d9
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:19:46 -0600

    Update DebugTwo4.cs

--- chapter2/de04/student/DebugTwo4.cs
+++ chapter2/de04/student/DebugTwo4.cs
@@ -2,20 +2,23 @@
 // one more and one less than the entered integer
 using System;
 using static System.Console;
-classDebugTwo4
+class DebugTwo4
 {
    static void Main()
    {
       string entry;
       int enteredInteger;
       int more, less;
+
       Write("Enter an integer >> ");
       entry = ReadLine();
       WriteLine("You entered {0}", entry);
-      enterdInteger = Convert.ToInt32(entry);
+      enteredInteger = Convert.ToInt32(entry);
+
       more = enteredInteger + 1;
       less = enteredInteger - 1;
-      WriteLine("One more than (0) is {1} and one less than {2} is {3}",
-	 enteredInteger, more less);
+
+      WriteLine("One more than {0} is {1} and one less than {0} is {2}",
+	 enteredInteger, more, less);
    }
 }
