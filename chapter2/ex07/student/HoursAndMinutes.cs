afda43b67f7717b58bc8c7fc4c96342024735aee
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:22:13 -0600

    Update HoursAndMinutes.cs

--- chapter2/ex07/student/HoursAndMinutes.cs
+++ chapter2/ex07/student/HoursAndMinutes.cs
@@ -5,6 +5,10 @@ class HoursAndMinutes
 {
 	static void Main()
 	{
-		// Write your code here
+		double minutes = 197;
+        int hours = (int) minutes / 60;
+        double minutesLeft = hours % 60;
+
+        WriteLine("{0} minutes is {1} hours and {2} minutes", minutes, hours, minutesLeft);
 	}
 }
