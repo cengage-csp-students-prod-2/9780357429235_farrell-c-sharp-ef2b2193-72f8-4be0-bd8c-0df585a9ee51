40f92a3226e47c581c179fd5b2187373d3a2c926
Mypham014 <hmyp014@gmail.com>
2025-02-11 23:23:48 -0600

    Update FahrenheitToCelsius.cs

--- chapter2/ex12/student/FahrenheitToCelsius.cs
+++ chapter2/ex12/student/FahrenheitToCelsius.cs
@@ -5,6 +5,11 @@ class FahrenheitToCelsius
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter the temperature in Fahrenheit >> ");
+        double fahrenheit = double.Parse(ReadLine());
+
+        double celsius = (fahrenheit - 32) * (double)(5.0 / 9.0);
+
+        WriteLine("{0} F is {1} C", fahrenheit.ToString("0.0"), celsius.ToString("0.0"));
 	}
 }
