7b32b01da3be785e022a4ccb302e4e766854accb
Mypham014 <hmyp014@gmail.com>
2025-02-23 19:46:45 -0600

    Create CheckCredit.cs

---
+++ chapter4/ex01/CheckCredit.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class CheckCredit
+{
+	static void Main()
+	{
+		Write("Please Enter the price purchase >> ");
+        double price = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+        if (price > 8000){
+          Write("You have exceeded the credit limit");
+        }
+        else{
+          Write("Approved");
+        }
+    
+	}
+}
