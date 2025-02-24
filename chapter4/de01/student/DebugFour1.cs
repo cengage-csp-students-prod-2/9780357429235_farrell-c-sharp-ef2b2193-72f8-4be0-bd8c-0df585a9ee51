b24dda3907a7f55a096005d8d69efbf2b732b075
Mypham014 <hmyp014@gmail.com>
2025-02-24 02:30:10 +0000

    Testing Mon Feb 24 2025 02:30:10 GMT+0000 (Coordinated Universal Time)

--- chapter4/de01/student/DebugFour1.cs
+++ chapter4/de01/student/DebugFour1.cs
@@ -10,22 +10,30 @@ class DebugFour1
       const double BASIC_DOG_PRICE = 2.00;
       const double CHILI_PRICE = 0.69;
       const double CHEESE_PRICE = 0.49;
+
       String wantChili, wantCheese;          
       double price;
+
       Write("Do you want chili on your dog? ");
-      wantChilli = ReadLine();
+      wantChili = ReadLine();
       Write("Do you want cheese on your dog? ");
       wantCheese = ReadLine();
-      if(wantChili = "Y")
-         if(wantCheese = "Y")
-            price == BASIC_DOG_PRICE + CHILI_PRICE + CHEESE_PRICE;
+
+      if(wantChili == "Y"){
+         if(wantCheese == "Y")
+            price = BASIC_DOG_PRICE + CHILI_PRICE + CHEESE_PRICE;
          else
-            price == BASIC_DOG_PRICE + CHILI_PRICE;
-      else
-         if(wantCheese = "Y")
+            price = BASIC_DOG_PRICE + CHILI_PRICE;
+      }
+
+      else{
+         if(wantCheese == "Y")
             price = BASIC_DOG_PRICE;
          else
-            price == BASIC_DOG_PRICE;
+            price = BASIC_DOG_PRICE;
+      }
+
       WriteLine("Your total is {0}", price.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
    }
 }
